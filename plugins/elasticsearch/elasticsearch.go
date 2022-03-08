8e3f96781e03c1d39fea607e8271a8644f1f225c
Shirley Gordon <shirley@Shirleys-MacBook-Pro.local>
2022-03-08 13:13:18 +0200

    TSK-2814: added elasticsearch plugin

--- plugins/elasticsearch/elasticsearch.go
+++ plugins/elasticsearch/elasticsearch.go
@@ -1,9 +1,14 @@
 package elasticsearch
 
 import (
+	"encoding/json"
+	"errors"
+	"github.com/blinkops/blink-http/consts"
 	"github.com/blinkops/blink-http/plugins/connections"
+	"github.com/blinkops/blink-http/plugins/types"
 	blink_conn "github.com/blinkops/blink-sdk/plugin/connections"
 	"net/http"
+	"strings"
 )
 
 type ElasticSearchPlugin struct{}
@@ -15,11 +20,54 @@ func (p ElasticSearchPlugin) HandleAuth(req *http.Request, conn map[string]strin
 }
 
 func (p ElasticSearchPlugin) TestConnection(connection *blink_conn.ConnectionInstance) (bool, []byte) {
-	return false, []byte("Test connection failed, Elasticsearch is not yet supported by the http plugin")
+	requestUrl, ok := connection.Data[consts.RequestUrlKey]
+	if !ok {
+		return false, []byte(consts.RequestUrlMissing)
+	}
+
+	res, err := connections.SendTestConnectionRequest(requestUrl, http.MethodGet, nil, connection, p.HandleAuth)
+	if err != nil {
+		return false, []byte(err.Error())
+	}
+
+	if res.StatusCode == http.StatusUnauthorized {
+		return false, []byte(consts.TestConnectionFailed)
+	}
+
+	res, err = p.ValidateResponse(res)
+	if err != nil {
+		return false, []byte(err.Error())
+	}
+
+	return true, nil
 }
 
 func (p ElasticSearchPlugin) GetDefaultRequestUrl() string {
 	return ""
+}
+
+func (p ElasticSearchPlugin) ValidateResponse(response *types.Response) (*types.Response, error) {
+	type ElasticSearchError struct {
+		Error *struct {
+			Reason       string `json:"reason"`
+		} `json:"error,omitempty"`
+	}
+
+	data := ElasticSearchError{}
+
+	err := json.Unmarshal(response.Body, &data)
+	if err != nil {
+		if strings.Contains(string(response.Body), "html") {
+			return nil, errors.New("invalid URL")
+		}
+		return nil, errors.New("invalid JSON from server")
+	}
+
+	if data.Error != nil {
+		return nil, errors.New(string(response.Body))
+	}
+
+	return nil, nil
 }
 
 func GetNewElasticSearchPlugin() ElasticSearchPlugin {
