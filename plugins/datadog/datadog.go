7cbc947d6644a1b5b1db7881c89f6b7720669372
Shirley Gordon <shirley@Shirleys-MacBook-Pro.local>
2022-03-08 13:11:06 +0200

    TSK-2814: added datadog test connection

--- plugins/datadog/datadog.go
+++ plugins/datadog/datadog.go
@@ -1,6 +1,8 @@
 package datadog
 
 import (
+	"fmt"
+	"github.com/blinkops/blink-http/consts"
 	"github.com/blinkops/blink-http/plugins/connections"
 	"github.com/blinkops/blink-http/plugins/types"
 	blink_conn "github.com/blinkops/blink-sdk/plugin/connections"
@@ -14,7 +16,21 @@ func (p DatadogPlugin) HandleAuth(req *http.Request, conn map[string]string) err
 }
 
 func (p DatadogPlugin) TestConnection(connection *blink_conn.ConnectionInstance) (bool, []byte) {
-	return false, []byte("Test connection failed, Datadog is not yet supported by the http plugin")
+	requestUrl, ok := connection.Data[consts.RequestUrlKey]
+	if !ok {
+		return false, []byte(consts.RequestUrlMissing)
+	}
+
+	res, err := connections.SendTestConnectionRequest(fmt.Sprintf("%s/api/v1/validate", requestUrl), http.MethodGet, nil, connection, p.HandleAuth)
+	if err != nil {
+		return false, []byte(err.Error())
+	}
+
+	if res.StatusCode == http.StatusForbidden {
+		return false, []byte(consts.TestConnectionFailed)
+	}
+
+	return true, nil
 }
 
 func (p DatadogPlugin) GetDefaultRequestUrl() string {
